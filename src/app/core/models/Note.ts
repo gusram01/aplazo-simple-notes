f9d28cfbaf8a2fd8d344b5ab4b7394b370ad12ff
Gus Ramírez <gustavo@coconutt.io>
2021-10-27 21:29:19 -0500

    feat: Note model and factory

---
+++ src/app/core/models/Note.ts
@@ -0,0 +1,30 @@
+import { nanoid } from 'nanoid';
+
+export class Note {
+  title: string;
+  content: string;
+  isArchived: boolean;
+  readonly id: string;
+  readonly createdAt: Date;
+
+  constructor(
+    id: string,
+    title: string,
+    content: string,
+    createdAt: Date,
+    isArchived: boolean
+  ) {
+    this.id = id;
+    this.title = title;
+    this.content = content;
+    this.createdAt = createdAt;
+    this.isArchived = isArchived;
+  }
+}
+
+export const factoryNote = (title: string, content: string) => {
+  const id = nanoid();
+  const currentDate = new Date();
+
+  return new Note(id, title, content, currentDate, false);
+};
