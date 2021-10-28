f1e00e6bc1a18056cc29748e6c1b1674640d5c1c
Gus Ramírez <gustavo@coconutt.io>
2021-10-27 22:39:12 -0500

    refactor: add mat card into layout

--- src/app/features/shared/layout/layout.module.ts
+++ src/app/features/shared/layout/layout.module.ts
@@ -3,12 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
 
 import { LayoutComponent } from './components/layout/layout.component';
 
 @NgModule({
   declarations: [LayoutComponent],
   exports: [LayoutComponent],
-  imports: [CommonModule, MatSidenavModule, MatToolbarModule],
+  imports: [CommonModule, MatSidenavModule, MatToolbarModule, MatCardModule],
 })
 export class LayoutModule {}
