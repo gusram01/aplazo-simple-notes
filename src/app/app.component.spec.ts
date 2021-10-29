d9715083e8967cbc60bbd8f813612ba0065c3c5e
Gus Ramírez <gustavo@coconutt.io>
2021-10-28 19:43:50 -0500

    test: storage service

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,10 +1,14 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AppRoutingModule } from './app-routing.module';
+import { CoreModule } from './core/core.module';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [AppComponent],
+      imports: [AppRoutingModule, CoreModule, RouterTestingModule],
     }).compileComponents();
   });
 
--- src/app/core/services/storage.service.spec.ts
+++ src/app/core/services/storage.service.spec.ts
@@ -1,16 +1,70 @@
 import { TestBed } from '@angular/core/testing';
+import { Note } from '../models/Note';
 
 import { StorageService } from './storage.service';
+
+const notes: Note[] = [
+  {
+    id: '1',
+    title: 'Note 1',
+    content: 'Note 1 content',
+    createdAt: new Date(),
+    isArchived: false,
+  },
+  {
+    id: '2',
+    title: 'Note 2',
+    content: 'Note 2 content',
+    createdAt: new Date(),
+    isArchived: false,
+  },
+];
+
+class MockStorage {
+  getNotes(): Promise<Note[]> {
+    return Promise.resolve(notes);
+  }
+  setNotes(note: Note): Promise<void> {
+    notes.push(note);
+    return Promise.resolve();
+  }
+}
 
 describe('StorageService', () => {
   let service: StorageService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [{ provider: StorageService, useClass: MockStorage }],
+    });
     service = TestBed.inject(StorageService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should get notes', () => {
+    spyOn(service, 'getNotes').and.callThrough();
+
+    service.getNotes();
+
+    expect(service.getNotes).toHaveBeenCalledTimes(1);
+  });
+
+  it('should set notes', () => {
+    const newNote: Note = {
+      id: '3',
+      title: 'Note 3',
+      content: 'Note 3 content',
+      createdAt: new Date(),
+      isArchived: false,
+    };
+
+    spyOn(service, 'setNotes').and.callThrough();
+
+    service.setNotes([...notes, newNote]);
+
+    expect(service.setNotes).toHaveBeenCalledTimes(1);
   });
 });
